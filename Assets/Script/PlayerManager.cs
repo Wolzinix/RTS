fc6cb3193236ae1eacac2f5fbf3a0eae15525d71
Wolzinix <wolzinix@outlook.fr>
2024-05-08 10:58:20 -0400

    feat: can moove entity

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -16,24 +16,43 @@ public class PlayerManager : MonoBehaviour
     void Start()
     {
         selectEntityInput.action.started += DoASelection;
+        mooveEntityInput.action.started += MooveSelected;
     }
 
-    private void OnDisable()
+    private void OnDestroy()
     {
         selectEntityInput.action.started -= DoASelection;
+        mooveEntityInput.action.started -= MooveSelected;
     }
     
     private void DoASelection(InputAction.CallbackContext context )
     {
-        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast (ray, out hit, 100)) {
-            if (hit.transform.GetComponent(typeof(NavMeshAgent)))
-            {
-                _selectedObject = hit.transform.gameObject;
-            }
+        RaycastHit hit = DoARayCast();
+        if (hit.transform.GetComponent<NavMeshAgent>())
+        {
+            _selectedObject = hit.transform.gameObject;
+        }
+        Debug.Log(_selectedObject);
+    }
+
+    private void MooveSelected(InputAction.CallbackContext context)
+    {
+        if (_selectedObject.GetComponent<NavMeshAgent>())
+        {
+            RaycastHit hit = DoARayCast();
+            _selectedObject.GetComponent<NavMeshAgent>().SetDestination(hit.point);
         }
     }
     
-    
+    private RaycastHit DoARayCast()
+    {
+        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast (ray, out hit, 100))
+        {
+            return hit;
+        }
+
+        return hit;
+    }
 }
