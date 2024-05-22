59e93778818201d80eeced7e39f89f41f4cae492
Wolzinix <wolzinix@outlook.fr>
2024-05-22 03:24:56 -0400

    feat: camera can zoom

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -15,33 +15,75 @@ public class PlayerManager : MonoBehaviour
     [SerializeField] private InputActionReference mooveCameraInput;
     [SerializeField] private InputActionReference rotateCameraInput;
 
-    [SerializeField] private float speed = 1; 
-
+    [SerializeField] private InputActionReference activeRotateCameraInput;
     
+    
+    [SerializeField] private InputActionReference zoomCameraInput;
+    
+    [SerializeField] private float speed = 1;
+
+    [SerializeField] private float speedOfZoom = 10;
+
+    private bool _rotationActivated = false;
+    private Camera _camera;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        _camera = Camera.main;
         selectEntityInput.action.started += DoASelection;
         mooveEntityInput.action.started += MooveSelected;
+        activeRotateCameraInput.action.performed += ChangeRotate;
+        zoomCameraInput.action.performed += Zoom;
+        activeRotateCameraInput.action.canceled += ChangeRotate;
+    }
+
+    private void Zoom(InputAction.CallbackContext obj)
+    {
+        if (_camera.transform.position.y >= 3 && zoomCameraInput.action.ReadValue<Vector2>().y >= 0)
+        {
+            _camera.transform.position += new Vector3(0
+                ,-zoomCameraInput.action.ReadValue<Vector2>().y / speedOfZoom,
+                zoomCameraInput.action.ReadValue<Vector2>().y / speedOfZoom)* (Time.deltaTime * speed);
+        }
+        
+        if (_camera.transform.position.y <= 8 && zoomCameraInput.action.ReadValue<Vector2>().y <= 0)
+        {
+            _camera.transform.position += new Vector3(0
+                ,-zoomCameraInput.action.ReadValue<Vector2>().y / speedOfZoom,
+                zoomCameraInput.action.ReadValue<Vector2>().y / speedOfZoom)* (Time.deltaTime * speed);
+        }
+    }
+
+    private void ChangeRotate(InputAction.CallbackContext obj)
+    {
+        if (obj.performed)
+        {
+            _rotationActivated = true;
+        }
+        if (obj.canceled)
+        {
+            _rotationActivated = false;
+        }
     }
 
     private void OnDestroy()
     {
         selectEntityInput.action.started -= DoASelection;
         mooveEntityInput.action.started -= MooveSelected;
+        activeRotateCameraInput.action.started -= ChangeRotate;
     }
 
 
     private void Update()
     {
-        Camera.main.transform.position += new Vector3(mooveCameraInput.action.ReadValue<Vector2>().x,0,mooveCameraInput.action.ReadValue<Vector2>().y) 
-                                          * (Time.deltaTime * speed);
+        _camera.transform.position += new Vector3(mooveCameraInput.action.ReadValue<Vector2>().x,0,mooveCameraInput.action.ReadValue<Vector2>().y) 
+                                      * (Time.deltaTime * speed);
         
         
-        var rotation = Camera.main.transform.rotation;
-        rotation = new Quaternion(  rotation.x, rotateCameraInput.action.ReadValue<Vector2>().x/180 +rotation.y , rotation.z, rotation.w) ;
-        
-        Camera.main.transform.rotation = rotation;
+
+        //if(_rotationActivated) RotateCamera();
     }
 
     private void DoASelection(InputAction.CallbackContext context )
@@ -72,7 +114,7 @@ public class PlayerManager : MonoBehaviour
     
     private RaycastHit DoARayCast()
     {
-        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+        Ray ray = _camera.ScreenPointToRay (Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast (ray, out hit, 100))
         {
@@ -82,8 +124,14 @@ public class PlayerManager : MonoBehaviour
         return hit;
     }
 
-    private void MooveCamera(InputAction.CallbackContext context)
+    private void RotateCamera()
     {
-        
+        // to do
+        var rotation = _camera.transform.rotation;
+
+        _camera.transform.rotation = new Quaternion(  rotation.x,
+                                                    rotateCameraInput.action.ReadValue<Vector2>().x/360 + rotation.y ,
+                                                    rotation.z,
+                                                    rotation.w);
     }
 }
