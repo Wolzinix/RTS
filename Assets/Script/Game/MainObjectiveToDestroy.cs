c5f3e6067d65c5e8ac48a47830006e81832c83e7
Wolzinix <wolzinix@outlook.fr>
2024-09-16 18:59:51 -0400

    feat: Final screen

--- Assets/Script/Game/MainObjectiveToDestroy.cs
+++ Assets/Script/Game/MainObjectiveToDestroy.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class MainObjectiveToDestroy : MonoBehaviour
 {
+    [SerializeField] GameObject _UI;
+
+    [SerializeField] GameObject _FinalScreen;
+
     private void OnDestroy()
     {
+        _UI.SetActive(false);
+        _FinalScreen.SetActive(true);
         Application.Quit();
     }
 }
