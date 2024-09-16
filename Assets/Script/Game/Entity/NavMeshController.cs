5524fd75b28a66f9c83cb7222d374c8b2d15acb8
Wolzinix <wolzinix@outlook.fr>
2024-09-16 18:07:53 -0400

    feat: navmehs keep warning distance

--- Assets/Script/Game/Entity/NavMeshController.cs
+++ Assets/Script/Game/Entity/NavMeshController.cs
@@ -8,10 +8,14 @@ public class NavMeshController : MonoBehaviour
 {
     private NavMeshAgent _navMesh;
 
+
     void Start()
-    {
+    { 
         _navMesh = GetComponent<NavMeshAgent>();
-        _navMesh.stoppingDistance = GetComponentInChildren<MeshRenderer>().bounds.size.x + GetComponentInChildren<MeshRenderer>().bounds.size.z;
+        MeshRenderer meshrender = GetComponentInChildren<MeshRenderer>();
+        _navMesh.stoppingDistance = meshrender.bounds.size.x + meshrender.bounds.size.z;
+
+        _navMesh.radius =( meshrender.bounds.size.x + meshrender.bounds.size.z) /2;
     }
     public bool isStillOnTrajet()
     {
@@ -23,13 +27,16 @@ public class NavMeshController : MonoBehaviour
         if(!notAtLocation())
         {
             StopPath();
-            if (GetComponent<Rigidbody>()) { GetComponent<Rigidbody>().velocity = Vector3.zero; }
+
+            if (GetComponent<Rigidbody>()) { GetComponent<Rigidbody>().velocity = Vector3.zero;
+                GetComponent<Rigidbody>().freezeRotation = true;
+            }
         }
     }
 
     public void GetNewPath(Vector3 point)
     {
-        if(!_navMesh.SetDestination(point))
+        if (!_navMesh.SetDestination(point))
         {
             NavMeshHit ClosestPoint;
             NavMeshPath NavPath = new NavMeshPath();
@@ -41,16 +48,18 @@ public class NavMeshController : MonoBehaviour
 
     public void ActualisePath(EntityManager target)
     {
+
         _navMesh.SetDestination(target.transform.position);
     }
 
     public void StopPath()
-    {
+    { 
         _navMesh.ResetPath();
     }
 
     public bool notAtLocation()
     {
+
         return _navMesh.remainingDistance > _navMesh.stoppingDistance;
     }
 }
