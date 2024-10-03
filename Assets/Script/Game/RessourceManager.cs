3b1b701e0e40764d2ce3c259257bc5bd9222d077
Wolzinix <wolzinix@outlook.fr>
2024-10-03 15:05:30 -0400

    feat: Add Ressource manager

---
+++ Assets/Script/Game/RessourceManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class RessourceManager : MonoBehaviour
+{
+    int _gold;
+    int _wood;
+
+    RessourceUi _ui;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ui = GetComponent<RessourceUi>();
+    }
+
+    public void AddGold(int gold)
+    {
+        _gold += gold;
+        if(GetComponent<ControlManager>())
+        {
+            _ui.AddGold(gold);
+        }
+    }
+
+    public void AddWood(int wood)
+    {
+        _wood += wood;
+        if (GetComponent<ControlManager>())
+        {
+            _ui.AddWood(-wood);
+        }
+    }
+}
