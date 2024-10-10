f7021d5b9f970c879484cf02759785e3a81e678c
Wolzinix <wolzinix@outlook.fr>
2024-10-10 15:09:25 -0400

    feat: IA don't group Builder

--- Assets/Script/IA/IABrain.cs
+++ Assets/Script/IA/IABrain.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -9,27 +8,21 @@ public class IABrain : MonoBehaviour
     private  Dictionary<BuildingController, BuildingStats> DicoOfBuilding;
 
     public delegate void NeedToSendToBuildingDelegate(BuildingStats building, Vector3 location);
-
     public static event NeedToSendToBuildingDelegate NeedToSendEntityToBuildingEvent;
-
     public static event NeedToSendToBuildingDelegate NeedToSendGroupToBuildingEvent;
 
     [SerializeField] private GameObject Objectif;
 
     private List<GroupManager> _ListOfGroup;
-
-    public string _ennemieTag;
-
+    private List<GroupManager> _ListOfGroupToSpawnEntity;
+    private List<GroupManager> _ListOfGroupToProtectBuilding;
+    private List<BuilderController> _ListOfBuilder;
     private int TailleDuGroupe = 3;
 
-    public int nbGroup;
-
-    private List<GroupManager> _ListOfGroupToSpawnEntity;
-
-    private List<GroupManager> _ListOfGroupToProtectBuilding;
-
+    public string _ennemieTag;
     private float DistanceOfSecurity = 3f;
 
+    public int nbGroup;
     public class BuildingStats
     {
         public List<GameObject> EntityNextTo = new List<GameObject>();
@@ -88,6 +81,7 @@ public class IABrain : MonoBehaviour
         _ListOfGroup = new List<GroupManager>();
         _ListOfGroupToSpawnEntity = new List<GroupManager>();
         _ListOfGroupToProtectBuilding = new List<GroupManager>();
+        _ListOfBuilder = new List<BuilderController> ();
 
         NeedToSendEntityToBuildingEvent += SendEntityToBuilding;
 
@@ -122,19 +116,7 @@ public class IABrain : MonoBehaviour
         return list;
     }
 
-    private void ActualiseBuilding()
-    {
-        BuildingController[] buildings = Resources.FindObjectsOfTypeAll(typeof(BuildingController)) as BuildingController[];
-        foreach (BuildingController building in buildings)
-        {
-            BuildingStats stats = new BuildingStats();
-            stats.Tag = building.tag;
-            stats.building = building;
-            building.EntityNextToEvent.AddListener(stats.changeHaveEntity);
-            DicoOfBuilding[building] = stats;
-        }
-        GetAllieBuilding();
-    }
+  
 
     private GameObject GetTheClosetEntityOfAPoint(Vector3 point)
     {
@@ -170,10 +152,55 @@ public class IABrain : MonoBehaviour
         return theClosetEntity;
     }
 
+    private void DebugGroup()
+    {
+        nbGroup = _ListOfGroup.Count;
+        foreach (var group in _ListOfGroup) { Debug.Log(group.getNumberOnGroup()); }
+        Debug.Log("---------------");
+    }
+
+
+    private void ClearUselessGroup()
+    {
+        List<GroupManager> groupManagers = new List<GroupManager>(_ListOfGroup);
+        foreach(GroupManager group in groupManagers)
+        {
+            if(group.getNumberOnGroup() == 0){ RemoveAGroup(group); }
+        }
+    }
+
+    private void RemoveAGroup(GroupManager groupToRemove)
+    {
+        if(_ListOfGroupToProtectBuilding.Contains(groupToRemove)) { _ListOfGroupToProtectBuilding.Remove(groupToRemove);  }
+        if(_ListOfGroupToSpawnEntity.Contains(groupToRemove)) { _ListOfGroupToSpawnEntity.Remove(groupToRemove); }
+        _ListOfGroup.Remove(groupToRemove);
+    }
+
+    private void SendToAttack(GroupManager group,GameObject objectif)
+    {
+        group.AttackingOnTravel(objectif.transform.position);
+    }
+
+    private void GroupToAttack(GroupManager group)
+    {
+        if (group.getNumberOnGroup() >= TailleDuGroupe && !_ListOfGroupToSpawnEntity.Contains(group) && !_ListOfGroupToProtectBuilding.Contains(group))
+        {
+            group.ResetOrder();
+            SendToAttack(group, Objectif);
+        }
+    }
+    private void SendEverybodyToTheCenter(GroupManager group)
+    {
+        foreach (EntityController entity in group.getSelectList())
+        {
+            entity.ClearAllOrder();
+            entity.AddPath(group.getCenterofGroup());
+        }
+    }
 
 
     public void SendRenfortToBuilding(BuildingStats building, Vector3 location)
-    { 
+    {
         if (gameObject.CompareTag(building.Tag) || building.Tag == "neutral")
         {
             GroupManager group = GetTheClosetGroupOfAPoint(location);
@@ -199,7 +226,7 @@ public class IABrain : MonoBehaviour
         }
     }
 
-    public void SendAGroup(GroupManager group,Vector3 point)
+    public void SendAGroup(GroupManager group, Vector3 point)
     {
         group.MooveSelected(point);
     }
@@ -222,58 +249,13 @@ public class IABrain : MonoBehaviour
             _ListOfGroupToSpawnEntity.Add(_ListOfGroup.Last());
             //DebugGroup();
         }
-       
+
     }
 
-
-    private void DebugGroup()
+    private void SendBuilderToHarvest()
     {
-        nbGroup = _ListOfGroup.Count;
-        foreach (var group in _ListOfGroup) { Debug.Log(group.getNumberOnGroup()); }
-        Debug.Log("---------------");
-    }
 
-
-    private void ClearUselessGroup()
-    {
-        List<GroupManager> groupManagers = new List<GroupManager>(_ListOfGroup);
-        foreach(GroupManager group in groupManagers)
-        {
-            if(group.getNumberOnGroup() == 0){ RemoveAGroup(group); }
-        }
     }
-
-    private void Update()
-    {
-        //StartCoroutine(ActualiseGroupAfter20S());
-    }
-
-    IEnumerator ActualiseGroupAfter20S()
-    {
-        yield return new WaitForSeconds(30f);
-        ActualiseGroup();
-    }
-    private void RemoveAGroup(GroupManager groupToRemove)
-    {
-        if(_ListOfGroupToProtectBuilding.Contains(groupToRemove)) { _ListOfGroupToProtectBuilding.Remove(groupToRemove);  }
-        if(_ListOfGroupToSpawnEntity.Contains(groupToRemove)) { _ListOfGroupToSpawnEntity.Remove(groupToRemove); }
-        _ListOfGroup.Remove(groupToRemove);
-    }
-
-    private void SendToAttack(GroupManager group,GameObject objectif)
-    {
-        group.AttackingOnTravel(objectif.transform.position);
-    }
-
-    private void GroupToAttack(GroupManager group)
-    {
-        if (group.getNumberOnGroup() >= TailleDuGroupe && !_ListOfGroupToSpawnEntity.Contains(group) && !_ListOfGroupToProtectBuilding.Contains(group))
-        {
-            group.ResetOrder();
-            SendToAttack(group, Objectif);
-        }
-    }
-
 
     private void AddEntityToGroup(GroupManager group, EntityController entity )
     {
@@ -286,14 +268,7 @@ public class IABrain : MonoBehaviour
         entity.GroupNumber = _ListOfGroup.IndexOf(group);
     }
 
-    private void SendEverybodyToTheCenter(GroupManager group)
-    {
-        foreach (EntityController entity in group.getSelectList())
-        {
-            entity.ClearAllOrder();
-            entity.AddPath(group.getCenterofGroup());
-        }
-    }
+   
 
     private bool EverybodyIsImmobile(GroupManager group)
     {
@@ -311,38 +286,45 @@ public class IABrain : MonoBehaviour
     {
         foreach (EntityController theCloset in groupOfEntity.GetComponentsInChildren<EntityController>())
         {
-            bool InGroup = false;
-            if (theCloset.CompareTag(gameObject.tag))
+            if(theCloset.GetComponent<BuilderController>())
             {
-                GroupManager groupeARejoindre = null;
-                if (_ListOfGroup.Count > 0)
+                _ListOfBuilder.Add(theCloset.GetComponent<BuilderController>());
+            }
+            else
+            {
+                bool InGroup = false;
+                if (theCloset.CompareTag(gameObject.tag))
                 {
-                    foreach (GroupManager group in _ListOfGroup)
-                    {
-                        if (group.GroupContainUnity(theCloset)) { InGroup = true;}
-                    }
-
-                    if (!InGroup)
+                    GroupManager groupeARejoindre = null;
+                    if (_ListOfGroup.Count > 0)
                     {
                         foreach (GroupManager group in _ListOfGroup)
                         {
-                            if (group.getNumberOnGroup() < TailleDuGroupe && !_ListOfGroupToSpawnEntity.Contains(group))
+                            if (group.GroupContainUnity(theCloset)) { InGroup = true; }
+                        }
+
+                        if (!InGroup)
+                        {
+                            foreach (GroupManager group in _ListOfGroup)
                             {
-                                if (groupeARejoindre == null) { groupeARejoindre = group; }
-                                else
+                                if (group.getNumberOnGroup() < TailleDuGroupe && !_ListOfGroupToSpawnEntity.Contains(group))
                                 {
-                                    if (Vector3.Distance(groupeARejoindre.getCenterofGroup(), theCloset.gameObject.transform.position) > Vector3.Distance(group.getCenterofGroup(), theCloset.gameObject.transform.position))
+                                    if (groupeARejoindre == null) { groupeARejoindre = group; }
+                                    else
                                     {
-                                        groupeARejoindre = group;
+                                        if (Vector3.Distance(groupeARejoindre.getCenterofGroup(), theCloset.gameObject.transform.position) > Vector3.Distance(group.getCenterofGroup(), theCloset.gameObject.transform.position))
+                                        {
+                                            groupeARejoindre = group;
+                                        }
                                     }
                                 }
                             }
+                            if (groupeARejoindre != null) { AddEntityToGroup(groupeARejoindre, theCloset); }
+                            else { Creategroup(theCloset.gameObject.GetComponent<TroupeManager>()); }
                         }
-                        if (groupeARejoindre != null)  { AddEntityToGroup(groupeARejoindre, theCloset); }
-                        else { Creategroup(theCloset.gameObject.GetComponent<TroupeManager>()); }
                     }
+                    else { Creategroup(theCloset.gameObject.GetComponent<TroupeManager>()); }
                 }
-                else { Creategroup(theCloset.gameObject.GetComponent<TroupeManager>()); }
             }
         }
         ClearUselessGroup();
@@ -358,6 +340,20 @@ public class IABrain : MonoBehaviour
         
     }
 
+    private void ActualiseBuilding()
+    {
+        BuildingController[] buildings = Resources.FindObjectsOfTypeAll(typeof(BuildingController)) as BuildingController[];
+        foreach (BuildingController building in buildings)
+        {
+            BuildingStats stats = new BuildingStats();
+            stats.Tag = building.tag;
+            stats.building = building;
+            building.EntityNextToEvent.AddListener(stats.changeHaveEntity);
+            DicoOfBuilding[building] = stats;
+        }
+        GetAllieBuilding();
+    }
+
     private void Creategroup(TroupeManager entity)
     {
         GroupManager groupToCreate = new GroupManager();
@@ -371,6 +367,8 @@ public class IABrain : MonoBehaviour
         groupToCreate.AddSelect(entity);
         entity.gameObject.GetComponent<EntityController>().GroupNumber = _ListOfGroup.IndexOf(groupToCreate);
     }
+
+
 
    
 }
