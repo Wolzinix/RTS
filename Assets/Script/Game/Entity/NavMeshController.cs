fc9740faeb86f0d3de92b8d6a41f2a6f54f5bad8
Wolzinix <wolzinix@outlook.fr>
2024-10-31 15:16:25 -0400

    feat: Entity now use RB for move

--- Assets/Script/Game/Entity/NavMeshController.cs
+++ Assets/Script/Game/Entity/NavMeshController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -5,13 +6,22 @@ public class NavMeshController : MonoBehaviour
 {
     private NavMeshAgent _navMesh;
 
+    private Rigidbody _rb;
+
+    private List<NavMeshAgent> _ListOfPosition = new List<NavMeshAgent>();
+
     void Start()
     { 
         _navMesh = GetComponent<NavMeshAgent>();
         MeshRenderer meshrender = GetComponentInChildren<MeshRenderer>();
+
+        _rb = GetComponent<Rigidbody>();
         _navMesh.stoppingDistance = meshrender.bounds.size.x + meshrender.bounds.size.z;
 
         _navMesh.radius =( meshrender.bounds.size.x + meshrender.bounds.size.z) /2;
+
+        _navMesh.updatePosition = false;
+        _navMesh.updateRotation = false;
     }
     public bool notOnTraject()
     {
@@ -27,15 +37,22 @@ public class NavMeshController : MonoBehaviour
 
     private void Update()
     {
-        if(!notAtLocation())
+
+        SetNextPosition();
+        if (!notAtLocation())
         {
             StopPath();
-            
             if (GetComponent<Rigidbody>()) { GetComponent<Rigidbody>().velocity = Vector3.zero;
                 GetComponent<Rigidbody>().freezeRotation = true;
             }
 
         }
+    }
+
+    private void SetNextPosition()
+    {
+        GetNewPath(_navMesh.destination);
+        _rb.MovePosition(new Vector3(_navMesh.nextPosition.x, _rb.position.y ,_navMesh.nextPosition.z));
     }
 
     public void GetNewPath(Vector3 point)
@@ -46,7 +63,6 @@ public class NavMeshController : MonoBehaviour
             NavMeshPath NavPath = new NavMeshPath();
             NavMesh.SamplePosition(point, out ClosestPoint, 200, 0);
             _navMesh.CalculatePath(ClosestPoint.position, NavPath);
-            _navMesh.SetPath(NavPath);
         }
     }
 
