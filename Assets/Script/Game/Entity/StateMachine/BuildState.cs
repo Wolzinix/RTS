abf00f4c8a18117083f462c0ec8b932d2d46e915
Wolzinix <wolzinix@outlook.fr>
2024-11-14 16:16:10 -0500

    feat: Add Build and harvest State

---
+++ Assets/Script/Game/Entity/StateMachine/BuildState.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class BuildState : StateClassEntity
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
