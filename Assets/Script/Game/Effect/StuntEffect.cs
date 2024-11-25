5f46c576fd323a3eea8c63d996266e95d1a92b9a
Wolzinix <wolzinix@outlook.fr>
2024-11-25 16:17:15 -0500

    feat: Add stunState

---
+++ Assets/Script/Game/Effect/StuntEffect.cs
@@ -0,0 +1,27 @@
+﻿public class StuntEffect : StateEffect
+{
+    public StuntEffect(SelectableManager entity, float duration) : base(entity, duration)
+    {
+        if (entityAffected.GetType() == typeof(TroupeManager))
+        {
+            if (!entityAffected.gotTheEffect(GetType())) { entity.AddEffect(this); }
+            else { entity.RemoveFirstEffectOfType(typeof(StuntEffect)); entity.AddEffect(this); }
+        }
+    }
+
+    public override void ApplyEffect()
+    {
+        if(entityAffected.GetComponent<EntityController>() != null)
+        {
+            entityAffected.GetComponent<EntityController>().AddStayOrder();
+        }
+    }
+
+    override public void end()
+    {
+        if (entityAffected.GetComponent<EntityController>() != null)
+        {
+            entityAffected.GetComponent<EntityController>().ClearAllOrder();
+        }
+    }
+}
