84f95f8b45fd52cedaa853ce9fb6003e05b082f2
Wolzinix <wolzinix@outlook.fr>
2025-01-25 17:10:47 -0500

    feat: Add kignht with sword mesh and animation

--- Assets/Script/Game/Entity/StateMachine/AttackState.cs
+++ Assets/Script/Game/Entity/StateMachine/AttackState.cs
@@ -69,7 +69,7 @@ public class AttackState : StateClassEntity
 
                 if (_animator.IsInTransition(0) == false &&
                    _animator.GetInteger(EntityController.Attacking) >= 1 &&
-                    _animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.5)
+                    _animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9)
 
                 { _animator.SetInteger(EntityController.Attacking, 0); }
 
