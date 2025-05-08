fca5a727496cc5894bb49543fb27d3721a88eddf
Wolzinix <wolzinix@outlook.fr>
2025-05-08 18:24:41 -0400

    feat: Ability have charge showned

--- Assets/Script/UI/OrderUiScript.cs
+++ Assets/Script/UI/OrderUiScript.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
@@ -5,6 +6,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using static UnityEditor.Experimental.GraphView.Port;
 
 public class OrderUiScript : MonoBehaviour
 {
@@ -15,14 +17,24 @@ public class OrderUiScript : MonoBehaviour
 
     [SerializeField] List<Button> _ListOfButton;
     [SerializeField] private InputActionReference RightClick;
+    private List<CapacityController> listOfCapacity = new();
 
     private void Start()
     {
         RightClick.action.started += DoRightClick;
     }
+    private void RemoveListenerFromCapacity()
+    {
+        foreach(CapacityController capacity in listOfCapacity)
+        {
+            capacity.ActivateEvent.RemoveListener(ActualiseACapacity);
+        }
+    }
     public void SetEntity(GameObject entity)
     {
         _entity = entity;
+        RemoveListenerFromCapacity();
+        listOfCapacity = _entity.GetComponentsInChildren<CapacityController>().ToList();
         ActualiseUi();
     }
 
@@ -33,12 +45,12 @@ public class OrderUiScript : MonoBehaviour
 
         if(_entity.GetComponent<TroupeManager>())
         {
-            List<CapacityController> listOfCapacaity = _entity.GetComponentsInChildren<CapacityController>().ToList();
+            StopAllCoroutines();
             foreach (Button button in _ListOfButton)
             {
-                if (_ListOfButton.IndexOf(button) < listOfCapacaity.Count)
+                if (_ListOfButton.IndexOf(button) < listOfCapacity.Count)
                 {
-                    CapacityController capacity = listOfCapacaity[_ListOfButton.IndexOf(button)];
+                    CapacityController capacity = listOfCapacity[_ListOfButton.IndexOf(button)];
 
                     button.gameObject.SetActive(true);
                     button.GetComponentInChildren<TMP_Text>().text = capacity.Name;
@@ -52,6 +64,8 @@ public class OrderUiScript : MonoBehaviour
                         button.GetComponent<Button>().enabled = true;
                         button.onClick.RemoveAllListeners();
                         button.onClick.AddListener(delegate { FindAnyObjectByType<ControlManager>().CapacityOrder(capacity); });
+                        StartCoroutine(ChargeBarOfAbility(button.GetComponentsInChildren<Image>()[1], capacity));
+                        capacity.ActivateEvent.AddListener(ActualiseACapacity);
                     }
                 }
                 else
@@ -72,6 +86,12 @@ public class OrderUiScript : MonoBehaviour
        
     }
 
+    private void ActualiseACapacity(CapacityController capacity)
+    {
+        Button button = _ListOfButton[listOfCapacity.IndexOf(capacity)];
+        StartCoroutine(ChargeBarOfAbility(button.GetComponentsInChildren<Image>()[1], capacity));
+    }
+
     public void GoToBuildUi()
     {
         _buildUi.gameObject.SetActive(true);
@@ -81,8 +101,8 @@ public class OrderUiScript : MonoBehaviour
 
     private List<RaycastResult> DoUiRayCast()
     {
-        PointerEventData eventData = new PointerEventData(EventSystem.current);
-        List<RaycastResult> results = new List<RaycastResult>();
+        PointerEventData eventData = new (EventSystem.current);
+        List<RaycastResult> results = new ();
         eventData.position = Input.mousePosition;
         EventSystem.current.RaycastAll(eventData, results);
 
@@ -108,5 +128,21 @@ public class OrderUiScript : MonoBehaviour
                 }
             }
         }
+    }
+
+    IEnumerator ChargeBarOfAbility(Image Imagebutton, CapacityController capacity)
+    {
+        float progressionValue =  capacity.actualTime / capacity.GetCooldown();
+        yield return new WaitForFixedUpdate();
+
+        while (progressionValue < 1 && capacity.actualTime != 0)
+        {
+            Imagebutton.fillAmount = progressionValue;
+            progressionValue = capacity.actualTime / capacity.GetCooldown();
+            yield return new WaitForEndOfFrame();
+        }
+
+        Imagebutton.fillAmount = 1;
+        yield return null;
     }
 }
