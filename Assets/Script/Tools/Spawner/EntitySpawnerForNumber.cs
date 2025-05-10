7d712cba2d2138f53bff2c99e60f999359eb106f
Wolzinix <wolzinix@outlook.fr>
2025-05-10 17:56:14 -0400

    feat: Add random timer for spawn

--- Assets/Script/Tools/Spawner/EntitySpawnerForNumber.cs
+++ Assets/Script/Tools/Spawner/EntitySpawnerForNumber.cs
@@ -1,6 +1,8 @@
 using Assets.Script.Tools;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class EntitySpawnerForNumber : MonoBehaviour
 {
@@ -8,19 +10,13 @@ public class EntitySpawnerForNumber : MonoBehaviour
     [SerializeField] private List<GameObject> _entityToSpawn;
     [SerializeField] private IABrain _ia;
     [SerializeField] private int NumberOfEntityToBeSpawn = 1;
-    private List<AggressifEntityManager> _entitysSpawn = new();
+    private int numberOfEntity;
     void Update()
     {
-        while (_entitysSpawn.Count < NumberOfEntityToBeSpawn)
+        while (numberOfEntity < NumberOfEntityToBeSpawn)
         {
-            GameObject go = Instantiate(_entityToSpawn[Random.Range(0, _entityToSpawn.Count)], transform.parent);
-            go.tag = gameObject.tag;
-            go.name = NameIndex.GetAName();
-            go.transform.SetPositionAndRotation(RayCast.RaycastForGround(go, gameObject.transform.position), transform.rotation);
-            AggressifEntityManager aggressifEntityManager = go.GetComponent<AggressifEntityManager>();
-            aggressifEntityManager.ActualiseSprite();
-            _entitysSpawn.Add(aggressifEntityManager);
-            aggressifEntityManager.deathEvent.AddListener(removeEntity);
+            numberOfEntity++;
+            StartCoroutine(SpawnOneEntity());
         }
     }
 
@@ -28,6 +24,24 @@ public class EntitySpawnerForNumber : MonoBehaviour
     {
         AggressifEntityManager aggressifEntityManager = entity.GetComponent<AggressifEntityManager>();
         aggressifEntityManager.deathEvent.RemoveListener(removeEntity);
-        _entitysSpawn.Remove(aggressifEntityManager);
+        numberOfEntity --;
+    }
+
+    private void SpawnEntity()
+    {
+        GameObject go = Instantiate(_entityToSpawn[Random.Range(0, _entityToSpawn.Count)], transform.parent);
+        go.tag = gameObject.tag;
+        go.name = NameIndex.GetAName();
+        go.transform.SetPositionAndRotation(RayCast.RaycastForGround(go, gameObject.transform.position), transform.rotation);
+        AggressifEntityManager aggressifEntityManager = go.GetComponent<AggressifEntityManager>();
+        aggressifEntityManager.ActualiseSprite();
+        aggressifEntityManager.deathEvent.AddListener(removeEntity);
+    }
+    IEnumerator SpawnOneEntity()
+    {
+        int RandomSecond = Random.Range(0, 10);
+        yield return new WaitForSeconds(RandomSecond);
+        SpawnEntity();
+        yield return null;
     }
 }
